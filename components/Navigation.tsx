a2ca5924f9b82053872100d2fd31ca40f5e31f4c
Kazuma Tomatsu <info@dero1to.com>
2025-11-22 21:43:48 +0900

    ナビゲーションコンポーネントをリファクタリングし、サスペンスを追加してローディング状態を管理

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,8 +2,9 @@
 
 import Link from 'next/link'
 import { useSearchParams, usePathname } from 'next/navigation'
+import { Suspense } from 'react'
 
-export default function Navigation() {
+function NavigationContent() {
   const searchParams = useSearchParams()
   const pathname = usePathname()
 
@@ -40,5 +41,26 @@ export default function Navigation() {
         </svg>
       </Link>
     </nav>
+  )
+}
+
+export default function Navigation() {
+  return (
+    <Suspense fallback={
+      <nav className="flex space-x-2">
+        <div className="p-2 rounded-lg text-gray-600 dark:text-gray-400">
+          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+            <path fillRule="evenodd" d="M8.161 2.58a1.875 1.875 0 011.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0121.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 01-1.676 0l-4.994-2.497a.375.375 0 00-.336 0l-3.868 1.935A1.875 1.875 0 012.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437zM9 6a.75.75 0 01.75.75v8.5a.75.75 0 01-1.5 0v-8.5A.75.75 0 019 6zm6.75 2.25a.75.75 0 00-1.5 0v8.5a.75.75 0 001.5 0v-8.5z" clipRule="evenodd" />
+          </svg>
+        </div>
+        <div className="p-2 rounded-lg text-gray-600 dark:text-gray-400">
+          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+            <path fillRule="evenodd" d="M2.625 6.75a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0A.75.75 0 018.25 6h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75zM2.625 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 12a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12A.75.75 0 017.5 12zm-4.875 5.25a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75z" clipRule="evenodd" />
+          </svg>
+        </div>
+      </nav>
+    }>
+      <NavigationContent />
+    </Suspense>
   )
 }
