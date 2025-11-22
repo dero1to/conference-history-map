5bac0cde0f88cbc30950da376e3984cc86fe303b
Kazuma Tomatsu <info@dero1to.com>
2025-11-22 20:40:45 +0900

    イベント名にリンクを追加し、イベントページを開く機能を実装

--- components/ConferenceList.tsx
+++ components/ConferenceList.tsx
@@ -53,8 +53,24 @@ export default function ConferenceList({ events, conferences }: ConferenceListPr
             return (
               <tr key={`${event.name}`} className="hover:bg-gray-50 dark:hover:bg-gray-800 transition-colors">
                 <td className="px-6 py-4 whitespace-nowrap">
-                  <div className="font-medium text-gray-900 dark:text-gray-100">
-                    {event.name}
+                  <div className="flex items-center gap-2">
+                    <span className="font-medium text-gray-900 dark:text-gray-100">
+                      {event.name}
+                    </span>
+                    {event.eventUrl && (
+                      <a
+                        href={event.eventUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-flex items-center text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300 transition-colors"
+                        title="イベントページを開く"
+                      >
+                        <svg className="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clipRule="evenodd" />
+                          <path fillRule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clipRule="evenodd" />
+                        </svg>
+                      </a>
+                    )}
                   </div>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
